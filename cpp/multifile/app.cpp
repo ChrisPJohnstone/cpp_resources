9f6426e355c5b8deb312e40f624a74d23bb38534
Christopher Johnstone <chrispjohnstone@gmail.com>
2023-11-06 20:12:28 +0000

    Added example of multifile code

---
+++ cpp/multifile/app.cpp
@@ -0,0 +1,7 @@
+#include "resource.h"
+
+// Note to self, you need to include all cpp file in compile (g++ command)
+int main() {
+    OtherFileFunction();
+    return 0;
+}
---
+++ cpp/multifile/resource.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "resource.h"
+
+void OtherFileFunction() {
+    std::cout << "Hello World!";
+}
